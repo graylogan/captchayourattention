b01787e15f596eb4248ffd32e12ac40e98f1da89
Logan Gray <lbg8fb@mst.edu>
2024-04-06 20:54:35 -0500

    please

--- game.js
+++ game.js
@@ -1,5 +1,5 @@
 (async () => {
-// set up PIXI 
+// set up PIXI lmaoooooo
 const app = new PIXI.Application();
 await app.init({ background: '#1099bb', resizeTo: window });
 document.body.appendChild(app.canvas);
